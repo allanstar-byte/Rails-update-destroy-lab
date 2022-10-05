57cb56fd258c900bdb32eb9724cb261740d5af5e
allanstar-byte <allanmutisya263@gmail.com>
2022-10-05 10:51:59 +0300

    Done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
-class PlantsController < ApplicationController
+class BirdsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -17,10 +18,34 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  
+  # PATCH /plants/:id
+  def update
+    plant =find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
 
+  # Delete
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+  
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+  private
+  #  Find helper
+  private
+  def find_plant
+    Plant.find(params[:id])
+  end
+  private
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
